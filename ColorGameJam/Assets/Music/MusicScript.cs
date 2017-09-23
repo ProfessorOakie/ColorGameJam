a7221efada11fde72b21eb5ac9a0d7d4c2b8c87c
Kylevw <Kyle@Kyles-MacBook-Pro.local>
2017-09-23 14:27:26 -0700

    The game plays music!! :O

---
+++ ColorGameJam/Assets/Music/MusicScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicScript : MonoBehaviour {
+
+	public AudioMixerSnapshot gameIntro;
+	public AudioMixerSnapshot gameMain;
+
+	public static float BPM = 124;
+
+	private float waitTime;
+
+	// Use this for initialization
+	void Start () {
+		waitTime = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		waitTime++;
+		if ((waitTime / 48) >= (16 / (BPM / 60))) {
+			gameMain.TransitionTo (0);
+		}
+	}
+}
