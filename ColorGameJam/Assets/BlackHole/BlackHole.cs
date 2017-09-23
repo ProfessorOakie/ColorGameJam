8cf6b3bcd8ffa710471b851e587d56c8fd661060
Drew Okenfuss <okenfuss@usc.edu>
2017-09-22 22:02:34 -0700

    Spawn zombie starter code

--- ColorGameJam/Assets/BlackHole/BlackHole.cs
+++ ColorGameJam/Assets/BlackHole/BlackHole.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackHole : MonoBehaviour {
+public class BlackHole : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private GameObject ZombiePrefab;
+    [SerializeField]
+    private float initialSpawnTime;
+    [SerializeField]
+    private float SpawnLoopTime;
+
+    // Use this for initialization
+    private void Start()
+    {
+        StartCoroutine(SpawnLoop(initialSpawnTime));
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    IEnumerator SpawnLoop(float time)
+    {
+        SpawnZombie();
+        yield return new WaitForSeconds(time);
+        //TODO: change spawn loop time logic
+        StartCoroutine(SpawnLoop(SpawnLoopTime));
+    }
+
+    private void SpawnZombie()
+    {
+        print("Zombie Was Spawned at time: " + Time.time + " from: " + gameObject.name);
+        //Instantiate(ZombiePrefab, transform.position, transform.rotation);
+    }
 }
