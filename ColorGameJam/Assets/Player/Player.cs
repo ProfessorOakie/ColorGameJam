5dc752603a5d1f5e920294fb64f459b1b1e82935
dolphinguy2014 <dolphinguy2014@msn.com>
2017-09-22 23:08:44 -0700

    Player Dammage
    Added the values for player dammage

--- ColorGameJam/Assets/Player/Player.cs
+++ ColorGameJam/Assets/Player/Player.cs
@@ -7,13 +7,30 @@ public class Player : MonoBehaviour {
     private Rigidbody2D rb;
     [SerializeField]
     private float MoveSpeed = 1;
+	[SerializeField]
+	private float StartingHealth = 100;
+	[SerializeField]
+	private float CurrentHealth;
+
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+		CurrentHealth = StartingHealth;
+	
 	}
 	
 	void Update () {
         Movement();
+		if (Input.GetKeyDown (KeyCode.I)) {
+			TakeDamage_Zombie (10);
+		}
+		if (Input.GetKeyDown (KeyCode.L)) {
+			TakeDamage_Bullet (5);
+		}
+		if (Input.GetKeyDown (KeyCode.B)) {
+			TakeDamage_BlackHole (15);
+		}
+
 	}
 
     private void Movement()
@@ -25,4 +42,40 @@ public class Player : MonoBehaviour {
         vec *= MoveSpeed;
         rb.velocity = vec;
     }
+
+	private void TakeDamage_Zombie (float amount)
+	{
+		CurrentHealth = CurrentHealth - amount;
+		if (CurrentHealth <= 0)
+		{
+			Dead();
+		}
+			
+	}
+
+	private void TakeDamage_Bullet (float amount)
+	{
+		CurrentHealth = CurrentHealth - amount;
+		if (CurrentHealth <= 0) 
+		{
+			Dead ();
+		}
+			
+	}
+
+	private void TakeDamage_BlackHole (float amount)
+	{
+		CurrentHealth = CurrentHealth - amount;
+		if (CurrentHealth <= 0)
+		{
+			Dead ();
+		}
+	}
+			
+	private void Dead ()
+	{
+		print ("I be ded");
+	
+	}
 }
+	
