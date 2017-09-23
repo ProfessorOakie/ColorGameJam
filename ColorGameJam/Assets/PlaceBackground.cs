b6044680c8d380284db0d47ff4a7b0f7419e7f2d
rachele175 <emig@usc.edu>
2017-09-23 14:26:04 -0700

    art. more art.

---
+++ ColorGameJam/Assets/PlaceBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBackground : MonoBehaviour {
+
+	public GameObject viewport;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (viewport.transform.position.x, viewport.transform.position.y, viewport.transform.position.y + 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
